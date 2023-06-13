7e43267fe832875c608fd418a3d15d7303372662
Amir Tadrisi <amirtds@gmail.com>
2023-06-13 16:06:01 -0500

    chore: change fontsize config

--- tutortailwind/templates/tailwind/lms/jstools/tailwind.config.js
+++ tutortailwind/templates/tailwind/lms/jstools/tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
       },
       height: {
         '120': '30rem',
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1.3rem',
+        xl: '1.5rem',
+        '2xl': '1.663rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
       }
     },
   },
