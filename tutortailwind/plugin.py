0a070d825be023df17dcb6fc6f137f420c0f133d
Amir Tadrisi <amirtds@gmail.com>
2025-04-01 17:01:50 -0500

    chore: remove plugin ENV_PATTERNS_INCLUDE

--- tutortailwind/plugin.py
+++ tutortailwind/plugin.py
@@ -43,49 +43,8 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 )
 
 # Force the rendering of scss files, even though they are included in a "partials" directory
-hooks.Filters.ENV_PATCHES.add_items(
-    [
-        # MFE will install header version 3.0.x and will include indigo-footer as a
-        # separate package for use in env.config.jsx
-        (
-            "mfe-dockerfile-post-npm-install-learning",
-            """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
-""",
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-authn",
-            """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
-""",
-        ),
-        # Tutor-Indigo v2.1 targets the styling updates in discussions and learner-dashboard MFE
-        # brand-openedx is related to styling updates while others are for header and footer updates
-        (
-            "mfe-dockerfile-post-npm-install-discussions",
-            """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
-""",
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-learner-dashboard",
-            """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
-""",
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-profile",
-            """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
-""",
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-account",
-            """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
-""",
-        ),
-    ]
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(
+    r"tailwind/lms/static/sass/partials/lms/theme/"
 )
 
 # Load all configuration entries
