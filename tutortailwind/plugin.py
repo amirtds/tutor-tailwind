cba906968f3cffbc249b0ff0c2739c1b693b214a
Amir Tadrisi <amirtds@gmail.com>
2025-04-01 19:33:49 -0500

    chore: update brand

--- tutortailwind/plugin.py
+++ tutortailwind/plugin.py
@@ -54,38 +54,38 @@ hooks.Filters.ENV_PATCHES.add_items(
         (
             "mfe-dockerfile-post-npm-install-learning",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#370ee526a3c3b921ca43738ea4766e8e536723b5'
 """,
         ),
         (
             "mfe-dockerfile-post-npm-install-authn",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'""",
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#370ee526a3c3b921ca43738ea4766e8e536723b5'""",
         ),
         # Tutor-Indigo v2.1 targets the styling updates in discussions and learner-dashboard MFE
         # brand-openedx is related to styling updates while others are for header and footer updates
         (
             "mfe-dockerfile-post-npm-install-discussions",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#370ee526a3c3b921ca43738ea4766e8e536723b5'
 """,
         ),
         (
             "mfe-dockerfile-post-npm-install-learner-dashboard",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#370ee526a3c3b921ca43738ea4766e8e536723b5'
 """,
         ),
         (
             "mfe-dockerfile-post-npm-install-profile",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#370ee526a3c3b921ca43738ea4766e8e536723b5'
 """,
         ),
         (
             "mfe-dockerfile-post-npm-install-account",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#1136437ff4f6468c69a9d6086eec55461d6a0572'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#370ee526a3c3b921ca43738ea4766e8e536723b5'
 """,
         ),
     ]
